b3cf1564284941fb58957481fdbbcc7d79737163
bshenoy <bshenoy12@gmail.com>
2022-08-22 19:30:24 +0530

    final commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/', methods=["POST","GET"])
 def main():
-    return "welcome to machine learning project"
+    return "welcome to  world of datascience and machine learning project"
 
 
 if __name__ =="__main__":
