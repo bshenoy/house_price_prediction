f987389b643e7c8264dc5d9eb9849991ff5abd2e
bshenoy <bshenoy12@gmail.com>
2022-09-06 15:11:21 +0530

    completed

--- demo.py
+++ demo.py
@@ -1,13 +1,15 @@
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
+from housing.config.configuration import Configuartion
 import sys
 
 def main():
     try:
         pipeline = Pipeline()
         pipeline.run_pipeline()
-        
+        # datavalidationconfig = Configuartion().get_data_validation_config()
+        # print(datavalidationconfig)
 
     except Exception as e:
         logging.error(str(e))
